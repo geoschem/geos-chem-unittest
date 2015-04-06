115c2941cdb6f1aa8ea8cb0e33014a0c14f23071
Lizzie Lundgren <elundgren@seas.harvard.edu>
2015-04-06 17:10:07 -0400

    Add copied run dir makefile and get run info perl scripts used by makefile.
    Signed-off-by: Lizzie Lundgren <elundgren@seas.harvard.edu>

---
+++ perl/getSim.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl -w
+
+#EOC
+#------------------------------------------------------------------------------
+#                  GEOS-Chem Global Chemical Transport Model                  !
+#------------------------------------------------------------------------------
+#BOP
+#
+# !MODULE: getMet.pl
+#
+# !DESCRIPTION:  Extracts the met source from the input.geos file
+#  and passes the result to the root-level Makefile.
+#\\
+#\\
+# !REMARKS:
+#  Designed for use with the Unit Test makefiles after copying a run directory.
+# 
+# !REVISION HISTORY: 
+#  12 Mar 2015 - E. Lundgren - Initial version
+#EOP
+#------------------------------------------------------------------------------
+#BOC
+#
+# !LOCAL VARIABLES:
+#
+
+# Scalars
+my $rundir  = "";
+my $line    = "";
+my $sim  = "";
+
+# Arrays
+my @linestrings = ();
+my @rundirstrings = ();
+
+# Grep for input.geos header
+$line       = qx( grep "GEOS-CHEM UNIT TEST SIMULATION" ./input.geos );
+chomp( $line );
+
+# Split by spaces
+@linestrings    = split( ' ', $line );
+
+# Extract the original run directory name
+$rundir          = "$linestrings[4]";
+
+# Split by _
+@rundirstrings  = split( '_', $rundir );
+
+# Place in runinfo
+$sim         = "$rundirstrings[2]";
+
+# Print the result so that the Makefile can grab it
+print "$sim";
+
+# Return normally
+exit(0);
+#EOC
