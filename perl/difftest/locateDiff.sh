4efd4f03c79938f560cd04702810b7d0ac7b0f0e
Lizzie Lundgren <elundgren@seas.harvard.edu>
2015-10-02 16:56:28 -0400

    Automatically insert filenames in DiffTest analysis shell scripts.
    Signed-off-by: Lizzie Lundgren <elundgren@seas.harvard.edu>

--- perl/difftest/locateDiff.sh
+++ perl/difftest/locateDiff.sh
@@ -7,12 +7,12 @@
 # E. Lundgren, August 2015
 
 # restart files
-RSTFILE1="./Dev/trac_rst.geosfp_4x5_Hg.201301010100.Dev"
-RSTFILE2="./Ref/trac_rst.geosfp_4x5_Hg.201301010100.Ref"
+RSTFILE1="./Dev/trac_rst.rundir.201301010100.Dev"
+RSTFILE2="./Ref/trac_rst.rundir.201301010100.Ref"
 
 # diagnostic files
-DIAGFILE1="./Dev/trac_avg.geosfp_4x5_Hg.201301010000.Dev"
-DIAGFILE2="./Ref/trac_avg.geosfp_4x5_Hg.201301010000.Ref"
+DIAGFILE1="./Dev/trac_avg.rundir.201301010000.Dev"
+DIAGFILE2="./Ref/trac_avg.rundir.201301010000.Ref"
 
 # destination files for differences
 RSTDIFF="logs/rst_diffs_all.log"
