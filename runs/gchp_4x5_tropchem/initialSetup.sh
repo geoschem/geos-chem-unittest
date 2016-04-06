d94927faae23af33c13f986388400367f2b69876
Matthew Yannetti <matthew.yannetti@gmail.com>
2016-04-06 16:49:09 -0400

    GCHP updates for Dev Kit.

--- runs/gchp_4x5_tropchem/initialSetup.sh
+++ runs/gchp_4x5_tropchem/initialSetup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # First time setup script - run only once!
 
-if [[ -L CodeDir || -L geos || -L MainDataDir || -L MetDir || -L ExtraDataDir ]] ; then
+if [[ -L CodeDir || -L geos || -L MainDataDir || -L MetDir ]] ; then
   echo "Soft links already set up"
   exit 1
 fi
@@ -21,11 +21,29 @@ if [[ $onOdyssey == "y" ]]; then
   baseDir='/n/seasasfs01/gcgrid'
   MetDir=$baseDir
   MainDataDir="$baseDir/data/ExtData/HEMCO"
-  ExtraDataDir="/n/home03/cakelle2/data"
+  echo "Valid INPUT resolution choices:"
+  echo "[N] Native (0.25x0.3125) (2015-07-01 to 2015-07-10)"
+  echo "[2] 2x2.5                (2012-05-01 to 2014-12-31)"
+  echo "[4] 4x5                  (2012-05-01 to 2015-07-31)"
+  read -p "Select an INPUT resolution for met data: " resChoice
+  if [[ $resChoice == "N" || $resChoice == "n" ]]; then
+    MetDir=$MetDir/GEOS_0.25x0.3125/GEOS_0.25x0.3125.d/GEOS_FP
+    cp ExtData_Native.rc ExtData.rc
+  elif [[ $resChoice == 4 ]]; then
+    MetDir=$MetDir/GEOS_4x5/GEOS_FP
+    cp ExtData_4x5.rc ExtData.rc
+  elif [[ $resChoice == 2 ]]; then
+    MetDir=$MetDir/GEOS_2x2.5_GEOS_5/GEOS_FP
+    cp ExtData_2x25.rc ExtData.rc
+  else
+    echo "Invalid resolution selected"
+    unlink CodeDir
+    unlink geos
+    exit 1
+  fi
 elif [[ $onOdyssey  == "n" ]]; then
   read -p "Enter path containing met data: " MetDir
   read -p "Enter path to HEMCO data directory: " MainDataDir
-  read -p "Enter path to extra data: " ExtraDataDir
 else
   echo "Invalid response given"
   unlink CodeDir
@@ -33,10 +51,9 @@ else
   exit 1
 fi
 
-if [[ -d $MetDir && -d $MainDataDir && -d $ExtraDataDir ]]; then
+if [[ -d $MetDir && -d $MainDataDir ]]; then
   ln -s $MetDir MetDir
   ln -s $MainDataDir MainDataDir
-  ln -s $ExtraDataDir ExtraDataDir
 else
   echo "Could not find target directories"
   unlink CodeDir
