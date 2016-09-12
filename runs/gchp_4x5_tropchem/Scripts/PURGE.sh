2b6db4c7d7c000d0ece4a201310e9aa9589e53ef
Lizzie Lundgren <elundgren@seas.harvard.edu>
2016-09-12 15:17:35 -0400

    Move PURGE.sh to GCHP Scripts/ subdirectory
    NOTE: PURGE.sh is not currently invoked in the Makefile or described on
    the wiki. It is stored in Scripts pending determination of what to do
    with it.
    
    Signed-off-by: Lizzie Lundgren <elundgren@seas.harvard.edu>

--- runs/gchp_4x5_tropchem/Scripts/PURGE.sh
+++ runs/gchp_4x5_tropchem/Scripts/PURGE.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 currDir=$PWD
+
+cd ..
 if [[ ! -e CodeDir ]]; then
   echo "First set up a softlink to your code directory named CodeDir, e.g."
   echo " ln -s path/to/code CodeDir"
   exit 1
 fi
-
-# Go to the script directory
-cd Scripts
+cd $currDir
 
 # Generate and run a temporary compilation script
 ./compile_int.sh purge $( readlink -f $currDir/CodeDir ) yes no no
@@ -17,8 +17,5 @@ if [[ $? -ne 0 ]]; then
   echo "Error in compile script!"
   exit 1
 fi
-
-# Return to current directory (no effect)
-cd $currDir
 
 exit 0
