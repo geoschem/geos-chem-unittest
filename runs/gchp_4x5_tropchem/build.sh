eee0fd0e00d9498aa155dcca31c30fc7733cd4a7
Lizzie Lundgren <elundgren@seas.harvard.edu>
2016-10-19 16:51:10 -0400

    Updates to the GCHP run directory for easy cleaning/compiling
    Changes in this commit:
    	(1) Replace all clean/compile shell scripts with single file
    	    build.sh. This program accepts one argument that indicates
                clean and/or build type. See the the build.sh header for
                details.
    
    	(2) Update the Makefile to use the new build.sh script such that
                cleaning and compiling source code is possible from the run
                directory.
    
    	(3) Replace use of a geos symlink with copying the executable to
                the run directory.
    
    	(4) Store all outdated clean/compile shell scripts in the new
                Archive directory for easy future reference.
    
    	(5) Add some additional functionality to the makefile, such
    	    as outputting the code repo information to
                last_build_code_info.log when compiling, and new make targets
                such as make superclean. See Makefile_GCHP for details.
    
    	(6) Fix bug in ifort 13 Odyssey bashrc where exporting CPATH
                was commented out. In theory, it should not be needed but
                the current settings of the bashrc without it are not currently
                adquate. This should be looked into in the future.
    
    Signed-off-by: Lizzie Lundgren <elundgren@seas.harvard.edu>

---
+++ runs/gchp_4x5_tropchem/build.sh
@@ -0,0 +1,186 @@
+#!/bin/bash
+
+#------------------------------------------------------------------------------
+#                  GEOS-Chem Global Chemical Transport Model                  !
+#------------------------------------------------------------------------------
+#BOP
+#
+# !MODULE: build.sh
+# 
+# !DESCRIPTION: Cleans and/or compiles a GEOS-Chem High Performance (GCHP) 
+#  source code. Accepts one keyword argument indicating combination of clean 
+#  and compile settings. 
+#\\
+#\\
+# !REMARKS:
+#  (1) Implemented arguments options include:
+#         clean_gc
+#         clean_nuclear
+#         clean_all
+#         clean_mapl
+#         compile_debug
+#         compile_standard
+#         compile_mapl
+#         compile_clean
+#  (2) To add a new one simply add an 'elif' block in the clean section
+#      and/or the compile section.
+#
+# !REVISION HISTORY: 
+#  19 Oct 2016 - E. Lundgren - Initial version
+#  Navigate to your unit tester directory and type 'gitk' at the prompt
+#  to browse the revision history.
+#EOP
+#------------------------------------------------------------------------------
+#BOC
+
+###############################
+###  Configurable Settings  ###
+###############################
+
+BASHRC=GCHP.ifort13.bashrc.odyssey
+export ESMF_COMPILER=intel
+export ESMF_COMM=openmpi
+
+# WARNING: Code changes are necessary if switching from openmp to mvapich:
+#   (1) In CodeDir/GCHP/GIGC.mk, comment out the OpenMPI line (L53) and 
+#       uncomment the MVAPICH line (55)
+#   (2) In CodeDir/GCHP/Makefile, change “export ESMF_COMM=openmpi” to 
+#       “export ESMF_COMM=mvapich2”
+# NOTE: eventually these changes will be automatic
+
+###############################
+###     General Setup       ###
+###############################
+
+# Set run directory
+runDir=$PWD
+
+# Error check
+if [[ ! -e CodeDir ]]; then
+  echo "First set up a softlink to your code directory named CodeDir, e.g."
+  echo " ln -s path/to/code CodeDir"
+  exit 1
+elif [[ $# == 0 ]]; then
+  echo "Must pass argument to compile.sh"
+  exit 1
+elif [[ ! -f ${BASHRC} ]]; then
+  echo "Bashrc file in build.sh does not exist"
+  exit 1
+fi
+
+# Source the bashrc
+source ${runDir}/${BASHRC}
+
+# Go to the source code directory
+cd ${runDir}/CodeDir
+
+###############################
+###       Clean             ###
+###############################
+
+# clean_gc
+if [[ $1 == "clean_gc" ]]; then
+    make HPC=yes realclean
+
+# clean_nuclear
+elif [[ $1 == "clean_nuclear" ]]; then
+    cd GCHP
+    make EXTERNAL_GRID=y DEVEL=y the_nuclear_option
+    cd ..
+
+# clean_all
+elif [[ $1 == "clean_all" ]]; then
+    make HPC=yes realclean
+    cd GCHP
+    make the_nuclear_option
+    cd ..
+
+# clean_mapl
+elif [[ $1 == "clean_mapl" ]]; then
+    make realclean
+    cd GCHP
+    make EXTERNAL_GRID=y  DEVEL=y       DEBUG=y   MET=geos-fp      \
+         GRID=4x5         NO_REDUCED=y  UCX=n     wipeout_fvdycore
+    make EXTERNAL_GRID=y  DEVEL=y       DEBUG=y   MET=geos-fp      \
+         GRID=4x5         NO_REDUCED=y  UCX=n     wipeout_mapl
+    cd ..
+
+# compile_debug
+elif [[ $1 == "compile_debug" ]]; then
+    cd GCHP
+    make clean
+    cd ..
+
+# compile_standard
+elif [[ $1 == "compile_standard" ]]; then
+    cd GCHP
+    make clean
+    cd ..
+
+# compile_mapl
+elif [[ $1 == "compile_mapl" ]]; then
+    make realclean
+    cd GCHP
+    make EXTERNAL_GRID=y  DEVEL=y       DEBUG=y   MET=geos-fp      \
+         GRID=4x5         NO_REDUCED=y  UCX=n     wipeout_fvdycore
+    make EXTERNAL_GRID=y  DEVEL=y       DEBUG=y   MET=geos-fp      \
+         GRID=4x5         NO_REDUCED=y  UCX=n     wipeout_mapl
+    cd ..
+
+# compile_clean
+elif [[ $1 == "compile_clean" ]]; then
+    make HPC=yes realclean
+    cd GCHP
+    make EXTERNAL_GRID=y  DEVEL=y  the_nuclear_option
+    cd ..
+
+else
+  echo "Argument passed to util.sh is not defined"
+  exit 1
+fi
+ # Remove executable, if it exists
+rm -f ${runDir}/CodeDir/bin/geos
+
+# Check CPATH
+echo "CPATH is: $CPATH"
+
+###############################
+###       Compile           ###
+###############################
+if [[ $1 == "compile_debug" ]]; then
+    make -j${SLURM_NTASKS} NC_DIAG=y   CHEM=tropchem  EXTERNAL_GRID=y  \
+                           DEBUG=y     DEVEL=y        TRACEBACK=y      \
+                           MET=geosfp  GRID=4x5       NO_REDUCED=y     \
+                           UCX=n       BOUNDS=y       FPEX=y           \
+                           EXTERNAL_FORCING=y         hpc
+elif [[ $1 == "compile_standard" ]] || \
+     [[ $1 == "compile_mapl"     ]] || \
+     [[ $1 == "compile_clean"    ]]; then
+    make -j${SLURM_NTASKS} NC_DIAG=y   CHEM=tropchem EXTERNAL_GRID=y   \
+                           DEBUG=y     DEVEL=y       TRACEBACK=y       \
+                           MET=geos-fp GRID=4x5      NO_REDUCED=y      \
+                           UCX=n       hpc
+fi
+
+###############################
+###       Cleanup           ###
+###############################
+# Change back to the run directory
+cd ${runDir}
+
+# Cleanup and quit
+if [[ -e ${runDir}/CodeDir/bin/geos ]]; then
+   echo '#############################################'
+   echo '### GCHP executable compiled succesfully! ###'
+   echo '#############################################'
+   cp CodeDir/bin/geos .
+else
+   echo '###################################################'
+   echo '### WARNING: GCHP executable does not yet exist ###'
+   echo '###################################################'
+   unset runDir
+fi
+
+unset runDir
+
+exit 0
--- runs/gchp_4x5_tropchem/initialSetup.sh
+++ runs/gchp_4x5_tropchem/initialSetup.sh
@@ -1,29 +1,60 @@
 #!/bin/bash
+
+#------------------------------------------------------------------------------
+#                  GEOS-Chem Global Chemical Transport Model                  !
+#------------------------------------------------------------------------------
+#BOP
+#
+# !MODULE: initialSetup.sh
+# 
+# !DESCRIPTION: Sets up symbolic links to data required by GCHP.
+#\\
+#\\
+# !REMARKS:
+#  (1) Use upon first time setup of the run directory.  
+#  (2) Subsequent usage requires deletion of the following symbolic links
+#         CodeDir
+#         MainDataDir
+#         MetDir
+#         RestartsDir
+#         ChemDataDir
+#
+# !REVISION HISTORY: 
+#  Navigate to your unit tester directory and type 'gitk' at the prompt
+#  to browse the revision history.
+#EOP
+#------------------------------------------------------------------------------
+#BOC
 # First time setup script - run only once!
 
-if [[ -L CodeDir || -L geos || -L MainDataDir || -L MetDir || -L RestartsDir || -L ChemDataDir ]] ; then
+# Check whether symlinks for source code, met, hemco, chem, and restarts 
+# already are set. If yes then exist since setup is not necessary. 
+if [[ -L CodeDir && -L geos && -L MainDataDir && -L MetDir && -L RestartsDir && -L ChemDataDir ]] ; then
   echo "Soft links already set up"
   exit 1
 fi
+
+# Prompt the user for source code directory and set symlink
 read -p "Enter path to code directory:" codePath
 if [[ ! -d $codePath ]]; then
   echo "Directory $codePath does not exist"
   exit 1
 fi
 ln -s $codePath CodeDir
-ln -s CodeDir/bin/geos geos
 
 # Replace the GEOS-Chem Classic Makefile with the GCHP Makefile
 if [[ -f Makefile_GCHP ]]; then
   mv Makefile_GCHP Makefile
 fi
 
-# Now set up paths to the meteorological data
+# Prompt the user on whether using Odyssey (Harvard)
 read -p "Are you on Odyssey [y/n]? " onOdyssey
-# Make lower-case
+
+# Automatically set met, hemco, chem, and restart paths if on Odyssey,
+# and ask the user what input met resolution to use.
 onOdyssey=$( echo "$onOdyssey" | tr '[:upper:]' '[:lower:]' )
 if [[ $onOdyssey == "y" ]]; then
-  baseDir='/n/seasasfs01/gcgrid'
+  baseDir='/n/holylfs/EXTERNAL_REPOS/GEOS-CHEM/gcgrid'
   MetDir=$baseDir
   MainDataDir="$baseDir/data/ExtData/HEMCO"
   RestartsDir="$baseDir/data/ExtData/NC_RESTARTS"
@@ -45,21 +76,24 @@ if [[ $onOdyssey == "y" ]]; then
   else
     echo "Invalid resolution selected"
     unlink CodeDir
-    unlink geos
     exit 1
   fi
+
+# If not on Odyssey, ask the user for the paths
 elif [[ $onOdyssey  == "n" ]]; then
   read -p "Enter path containing met data: " MetDir
   read -p "Enter path to HEMCO data directory: " MainDataDir
   read -p "Enter path to GEOS-Chem restart files: " RestartsDir
   read -p "Enter path to CHEM_INPUTS directory for Olson maps: " ChemDataDir
+  echo "WARNING: you must replace ExtData.rc with the appropriate template"
+  echo "         file (e.g. ExtData_4x5.rc if using 4x5 met input res"
 else
   echo "Invalid response given"
   unlink CodeDir
-  unlink geos
   exit 1
 fi
 
+# Set symlinks based on the paths set above
 if [[ -d $MetDir && -d $MainDataDir && -d $RestartsDir && -d $ChemDataDir ]]; then
   ln -s $MetDir MetDir
   ln -s $MainDataDir MainDataDir
