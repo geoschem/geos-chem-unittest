6a6e1fa13e1df00d44cd573ba4a9497746c446df
Lizzie Lundgren <elundgren@seas.harvard.edu>
2016-12-02 17:49:47 -0500

    Provide instructions to GCHP users following execution of initialSetup.sh
    Signed-off-by: Lizzie Lundgren <elundgren@seas.harvard.edu>

--- runs/gchp_4x5_tropchem/initialSetup.sh
+++ runs/gchp_4x5_tropchem/initialSetup.sh
@@ -29,7 +29,8 @@
 
 # Check whether symlinks for source code, met, hemco, chem, and restarts 
 # already are set. If yes then exist since setup is not necessary. 
-if [[ -L CodeDir && -L MainDataDir && -L MetDir && -L RestartsDir && -L ChemDataDir ]] ; then
+tileFile=DE1440xPE0720_CF0024x6C.bin
+if [[ -L CodeDir && -L MainDataDir && -L MetDir && -L RestartsDir && -L ChemDataDir && -L ${tileFile}]] ; then
   echo "All necessary symbolic links already exist"
   exit 1
 fi
@@ -49,7 +50,6 @@ if [[ ! -d $tilePath ]]; then
   echo "Directory $tilePath does not exist"
   exit 1
 fi
-tileFile=DE1440xPE0720_CF0024x6C.bin
 ln -s ${tilePath}/${tileFile} ${tileFile} 
 
 # Prompt the user on whether using Odyssey (Harvard)
@@ -72,7 +72,6 @@ if [[ $onOdyssey == "y" ]]; then
   if [[ $resChoice == "N" || $resChoice == "n" ]]; then
     MetDir=$MetDir/GEOS_0.25x0.3125/GEOS_0.25x0.3125.d/GEOS_FP
     cp ExtData_Native.rc ExtData.rc
-    echo "WARNING: tile file needed for dry deposition does not exist yet!"
   elif [[ $resChoice == 4 ]]; then
     MetDir=$MetDir/GEOS_4x5/GEOS_FP
     cp ExtData_4x5.rc ExtData.rc
@@ -84,8 +83,6 @@ if [[ $onOdyssey == "y" ]]; then
     unlink CodeDir
     exit 1
   fi
-  echo "Odyssey run script GCHP.run is included in this directory."
-  echo "Open it and update the bashrc and your email address prior to use."
 
 # If not on Odyssey, ask the user for the paths
 elif [[ $onOdyssey  == "n" ]]; then
@@ -95,8 +92,6 @@ elif [[ $onOdyssey  == "n" ]]; then
   read -p "Enter path to CHEM_INPUTS directory for Olson maps: " ChemDataDir
   echo "WARNING: You must replace ExtData.rc with the appropriate template"
   echo "         file (e.g. ExtData_4x5.rc if using 4x5 met input resolution!"
-  echo "         You also must set up your environment. See sample bashrc files."
-  echo "         See also sample run script GCHP.run if your system uses SLURM."
 else
   echo "Invalid response given"
   unlink CodeDir
@@ -115,5 +110,26 @@ else
   unlink geos
   exit 1
 fi
-
+echo " "
+echo "IMPORTANT NOTE: You must now set up your environment!" 
+echo " "
+echo "  (1) Sample bashrc files that are compatible with the Harvard Odyssey"
+echo "      Compute Cluster are included in this run directory. If you are on a"
+echo "      different system, you may use them as a guide to customize a bashrc"
+echo "      compatible with your local system. By default, the Makefile and"
+echo "      GCHP.run are set to use GCHP.ifort15_openmpi_odyssey.bashrc."
+echo " "
+echo "  (2) Compile using build.sh, either directly or using the Makefile."
+echo "      Type './build.sh help' for options."
+echo " "
+echo "  (3) Sample run script GCHP.run is included in this directory."
+echo "      It is compatible with SLURM on the Harvard Odyssey Cluster"
+echo "      and may be adapted to other systems. Prior to use, open the file,"
+echo "      enter your email address for SLURM notifications, and update the"
+echo "      bashrc to match your environment. The bashrc specific in the"
+echo "      the Makefile is only invoked if you choose to run GCHP "
+echo "      interactively using make."
+echo " "
+echo "Thank you for using the GCHP Dev Kit! Please send any comments, issues, "
+echo "or questions to Lizzie Lundgren at elundgren@seas.harvard.edu"
 exit 0
