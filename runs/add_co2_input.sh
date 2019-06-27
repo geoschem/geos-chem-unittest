d6091f3f129252d1cdeae55205da1344d52e9314
Melissa Sulprizio <mpayer@seas.harvard.edu>
2019-06-27 13:27:55 -0400

    Remove shell script used for CO2 updates
    Signed-off-by: Melissa Sulprizio <mpayer@seas.harvard.edu>

--- runs/add_co2_input.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for dir in */; do
-	cd $dir
-	sed -i '/Wet Deposition/a Turn on CO2 Effect?     : F\nCO2 level               : 600.0\nReference CO2 level     : 380.0' input.geos.template
-	cd ..
-done
